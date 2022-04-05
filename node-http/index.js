997bf78485054437a973d2b3d717865a5c0c27f3
Ridwan <ridwanediallo99@gmail.com>
2022-04-05 15:32:17 +0000

    Add example of server with node js

--- node-http/index.js
+++ node-http/index.js
@@ -1,16 +1,107 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
+//     console.log('Request for ' + req.url + 'by method' + req.method)
 
-    res.statusCode = 200;
+//     if (req.method === 'GET') {
+//       var fileUrl;
+//       if (req.url === '/') fileUrl = './index.html';
+//       else fileUrl = req.url;
+
+//       var fileUrl = path.resolve('./public' + fileUrl);
+//       const fileExt = path.extname(fileUrl);
+//       if (fileExt === '.html') {
+//         fs.exists(fileUrl, (exists) => {
+//           if (!exists) {
+//             res.statusCode = 404;
+//             res.setHeader('Content-Type', 'text/html');
+//             res.end(
+//               `<html><body><h1>Error 404: ${fileUrl} no found</h1></body></html>`
+//             );
+//             return;
+//           }
+//           res.statusCode = 200;
+//           res.setHeader('Content-Type', 'text/html');
+//           fs.createReadStream(filePath).pipe(res);
+//         });
+//       } else {
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end(
+//           `<html><body><h1>Error 404: ${fileUrl} no an HTML file</h1></body></html>`
+//         );
+//         return;
+//       }
+//     } else {
+//       res.statusCode = 404;
+//       res.setHeader('Content-Type', 'text/html');
+//       res.end(
+//         `<html><body><h1>Error 404: ${req.method} no supported</h1></body></html>`
+//       );
+//       return;
+//     }
+
+// })
+
+const server = http.createServer((req, res) => {
+  console.log('Request for ' + req.url + ' by method ' + req.method);
+
+  if (req.method == 'GET') {
+    var fileUrl;
+    if (req.url == '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
+
+    var filePath = path.resolve('./public' + fileUrl);
+    const fileExt = path.extname(filePath);
+    if (fileExt == '.html') {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(
+            '<html><body><h1>Error 404: ' +
+              fileUrl +
+              ' not found</h1></body></html>'
+          );
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(
+        '<html><body><h1>Error 404: ' +
+          fileUrl +
+          ' not a HTML file</h1></body></html>'
+      );
+    }
+  } else {
+    res.statusCode = 404;
     res.setHeader('Content-Type', 'text/html');
-    res.end(<html><body><h1>Hello, world!</h1></body></html>);
-})
+    res.end(
+      '<html><body><h1>Error 404: ' +
+        req.method +
+        ' not supported</h1></body></html>'
+    );
+  }
+});
 
 
 server.listen(port, hostname, () => {
     console.log(`Server running ar http://${hostname}:${port}`);
 })
+
+
+
+
+
+
