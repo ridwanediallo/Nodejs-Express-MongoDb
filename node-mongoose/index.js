862fc0af3f0fec3151c2363dc41fd7565523cd1e
Ridwan <ridwanediallo99@gmail.com>
2022-06-24 21:09:25 +0000

    Add Mongoose ODM Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,27 +8,24 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log('Connected correctly to server');
 
-  var newDish = Dishes({
-    name: 'Uthappizza',
-    description: 'test',
-  });
+   Dishes.create({
+     name: 'Uthapizza',
+     description: 'Test',
+   })
+     .then((dish) => {
+       console.log(dish);
 
-  newDish
-    .save()
-    .then((dish) => {
-      console.log(dish);
+       return Dishes.find({}).exec();
+     })
+     .then((dishes) => {
+       console.log(dishes);
 
-      return Dishes.find({});
-    })
-    .then((dishes) => {
-      console.log(dishes);
-
-      return Dishes.remove({});
-    })
-    .then(() => {
-      return mongoose.connection.close();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+       return Dishes.remove({});
+     })
+     .then(() => {
+       return mongoose.connection.close();
+     })
+     .catch((err) => {
+       console.log(err);
+     });
 });
