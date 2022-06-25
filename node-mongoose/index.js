69039b4562d656db1e667730b356921275f4b8a3
Ridwan <ridwanediallo99@gmail.com>
2022-06-25 09:45:57 +0000

    Add Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,24 +8,40 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
   console.log('Connected correctly to server');
 
-   Dishes.create({
-     name: 'Uthapizza',
-     description: 'Test',
-   })
-     .then((dish) => {
-       console.log(dish);
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
 
-       return Dishes.find({}).exec();
-     })
-     .then((dishes) => {
-       console.log(dishes);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
 
-       return Dishes.remove({});
-     })
-     .then(() => {
-       return mongoose.connection.close();
-     })
-     .catch((err) => {
-       console.log(err);
-     });
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
