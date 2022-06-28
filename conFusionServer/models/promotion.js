e90ae7868246ef843f126241f2a9688abb4c25d9
Ridwan <ridwanediallo99@gmail.com>
2022-06-28 19:36:35 +0000

    Add promotion.js

---
+++ conFusionServer/models/promotion.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
+const promoSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Promotions = mongoose.model("Promo", promoSchema);
+
+module.exports = Promotions;
