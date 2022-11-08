4d51e2c2cad763bc0bdfffc76f80334068179292
ridwan <ridwanediallo99@gmail.com>
2022-11-08 00:11:06 +0000

    Replace var with const and let key words

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,18 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+let session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 
@@ -30,7 +29,7 @@ connect.then(
   }
 );
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,15 +40,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
-
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+app.use(
+  session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -58,14 +57,14 @@ function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var err = new Error('You are not authenticated!');
+    let err = new Error('You are not authenticated!');
     err.status = 401;
     return next(err);
   } else {
     if (req.session.user === 'authenticated') {
       next();
     } else {
-      var err = new Error('You are not authenticated!');
+      let err = new Error('You are not authenticated!');
       err.status = 403;
       return next(err);
     }
@@ -75,7 +74,6 @@ function auth(req, res, next) {
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
