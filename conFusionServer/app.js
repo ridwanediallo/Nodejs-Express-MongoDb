358e6ed007508ab64a4ae3950ffe3bb4b3e79d1a
ridwan <ridwanediallo99@gmail.com>
2022-11-22 10:11:45 +0000

    Update app.js to use passport authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 let session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -50,24 +51,20 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
     return next(err);
   } else {
-    if (req.session.user === 'authenticated') {
       next();
-    } else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
   }
 }
 
