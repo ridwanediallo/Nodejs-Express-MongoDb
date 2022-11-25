8d048dd9c9e83bbc97ff1e377f11b55f31d6c4c9
ridwan <ridwanediallo99@gmail.com>
2022-11-25 11:29:37 +0000

    Add Jwt authentication in authenticate.js file

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -1,7 +1,38 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var User = require('./models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+const config = require('./config');
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+exports.getToken = (user) => {
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+};
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = config.secretKey;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("Jwt payload: ", jwt_payload);
+    User.findOne({_id: jwt_payload._id}, (err, user) => {
+        if(err) {
+            return done(err, false);
+        }
+        else if(user) {
+            return done(null, user);
+        }
+        else {
+            return done(null, false);
+        }
+    })
+}))
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
