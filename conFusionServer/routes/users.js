73a004bb1ec854d579bf28cb5dcaf7301b4bfcc7
ridwan <ridwanediallo99@gmail.com>
2022-11-25 11:32:01 +0000

    Add token base authentication on login route

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+const authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -32,9 +33,11 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  const token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, status: 'You are Successful logged in!' });
+  res.json({ success: true, token: token, status: 'You are Successful logged in!' });
 });
 
 router.get('/logout', (req, res) => {
