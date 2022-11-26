4c24c69a2c08d2541aba5f5e30210e7c4a04669f
ridwan <ridwanediallo99@gmail.com>
2022-11-26 18:27:41 +0000

    Add firstname and lastname field into use schema

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -1,12 +1,20 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var passportLocalMongoose = require('passport-local-mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-var User = new Schema({
-    admin: {
-        type: Boolean,
-        default: false,
-    }
+const User = new Schema({
+  firstname: {
+    type: String,
+    default: '',
+  },
+  lastname: {
+    type: String,
+    default: '',
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 User.plugin(passportLocalMongoose);
