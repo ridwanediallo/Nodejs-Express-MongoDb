886baa558bd761529199284c0cc5fde9f0077919
ridwan <ridwanediallo99@gmail.com>
2022-11-26 18:38:05 +0000

    Update signup route

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -22,10 +22,23 @@ router.post('/signup', (req, res, next) => {
         res.setHeader('Content-Type', 'application/json');
         res.json({ err: err });
       } else {
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful!' });
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+              success: true,
+              status: 'Registration Successful!',
+            });
+          });
         });
       }
     }
@@ -33,11 +46,14 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-
-  const token = authenticate.getToken({_id: req.user._id});
+  const token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, token: token, status: 'You are Successful logged in!' });
+  res.json({
+    success: true,
+    token: token,
+    status: 'You are Successful logged in!',
+  });
 });
 
 router.get('/logout', (req, res) => {
